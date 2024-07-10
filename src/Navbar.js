04d3e61c3dd6a52e86855cbb41a4c19fbf826105
Elvin2605 <145311033+Elvin2605@users.noreply.github.com>
2024-07-10 15:48:36 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="Navbar">
+            <h1>The Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/Create" >New</a>
+            </div>
+        </nav>
+            
+     );
+}
+ 
+export default Navbar;
